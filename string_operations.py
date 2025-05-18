46ff342c1791b963d520bf07b82bd160d674b1b5
jhanvidattani25 <jhanvidattani@gmail.com>
2025-05-18 12:40:38 +0530

    Update string_operations.py

--- string_operations.py
+++ string_operations.py
@@ -3,11 +3,7 @@ def string_operations(string):
     print(f"Lower case: {string.lower()}")
     print(f"Upper case: {string.upper()}")
     print(f"Length: {len(string)}")
-    print(f"Reversed: {string[::-1]}")
-    print(f"Title case: {string.title()}")
-    print(f"Is alphanumeric: {string.isalnum()}")
-    print(f"Is digit: {string.isdigit()}")
-    print(f"Is alpha: {string.isalpha()}")
+
 
 if __name__ == "__main__":
     string = "test"
